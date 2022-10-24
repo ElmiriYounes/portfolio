bc24f588796bda60691dfc1d9e53271f58459aef
elmiri younes <elmiri.younes@hotmail.com>
2022-10-24 14:08:09 +0200

    rectificaiotn of some texts

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -14,7 +14,7 @@ const About: FC = () => {
   return (
     <AboutWrap>
       <SubTitle>
-        T<span>r</span>ained
+        T<span>r</span>aining
       </SubTitle>
       <p>
         Hi, my name is Younes El miri and I'm a web developer trainee at <b>Becode</b>. <b>Becode</b> provides
@@ -27,7 +27,7 @@ const About: FC = () => {
       </SubTitle>
       <p>
         I'm passionate about web coding. I continue to learn coding by myself
-        and I love to discovery new technologies. My favorites tech are{" "}
+        and I love to discover new technologies. My favorites tech are{" "}
         <b>ReactJS</b>, <b>NodeJS</b> and <b>ThreeJS</b> for 3D effects.
       </p>
       <SubTitle>
